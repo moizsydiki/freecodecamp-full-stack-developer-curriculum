584aaba31249618830fa45b3b6eefffc8c11bc95
Moiz Siddiqui <moizsidd05@gmail.com>
2025-11-25 23:53:19 +0500

    coded a lab in array section string inverter

---
+++ JavaScript/Arrays/LunchPicker.js
@@ -0,0 +1,52 @@
+const lunches = [];
+
+function addLunchToEnd(arr, str) {
+  arr.push(str);
+  console.log(`${str} added to the end of the lunch menu.`);
+  return arr;
+}
+
+function addLunchToStart(arr, str) {
+  arr.unshift(str);
+  console.log(`${str} added to the start of the lunch menu.`);
+  return arr;
+}
+
+function removeLastLunch(arr) {
+  removed = arr.pop();
+  if (removed) {
+    console.log(`${removed} removed from the end of the lunch menu.`);
+    return arr;
+  } else {
+    console.log("No lunches to remove.");
+  }
+}
+
+function removeFirstLunch(arr) {
+  removed = arr.shift();
+  if (removed) {
+    console.log(`${removed} removed from the start of the lunch menu.`);
+  } else {
+    console.log("No lunches to remove.");
+  }
+  return arr;
+}
+
+function getRandomLunch(arr) {
+  randomItem = arr[Math.floor(Math.random() * arr.length)];
+  if (arr.length >= 1) {
+    console.log(`Randomly selected lunch: ${randomItem}`);
+  } else {
+    console.log("No lunches available.");
+  }
+}
+
+function showLunchMenu(arr) {
+  if (arr.length >= 1) {
+    return console.log(`Menu items: ${arr.join(", ")}`);
+  } else {
+    return console.log("The menu is empty.");
+  }
+}
+
+console.log(removeLastLunch(["Sushi", "Pizza", "Noodles"]));
--- JavaScript/Arrays/ReversedStrings.js
+++
@@ -1,6 +0,0 @@
-const str = "zainab";
-const strSplitted = str.split("");
-const strReversed = strSplitted.reverse();
-const result = strReversed.join("");
-
-console.log(result);
