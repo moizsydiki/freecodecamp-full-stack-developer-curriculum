b267d48393794069e76e2a6a735b2e0a780cbb3b
Moiz Siddiqui <moizsidd05@gmail.com>
2025-12-26 20:55:44 +0500

    coded string inverter and largest number finder

---
+++ JavaScript/JavaScript Fundamentals Review/LargestNumberFinder.js
@@ -0,0 +1,11 @@
+function largestOfAll(arr) {
+  let largest = arr[0];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > largest) {
+      largest = arr[i];
+    }
+  }
+  return largest;
+}
+
+console.log(largestOfAll([9, 21, 41, 6, 22]));
