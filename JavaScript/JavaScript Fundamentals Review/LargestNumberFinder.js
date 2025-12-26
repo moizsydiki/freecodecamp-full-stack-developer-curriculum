edf4ae0f6581b21b7f494f3f6109b1575f47088a
Moiz Siddiqui <moizsidd05@gmail.com>
2025-12-26 21:34:40 +0500

    completed largest number finder lab

--- JavaScript/JavaScript Fundamentals Review/LargestNumberFinder.js
+++ JavaScript/JavaScript Fundamentals Review/LargestNumberFinder.js
@@ -1,11 +1,23 @@
 function largestOfAll(arr) {
-  let largest = arr[0];
+  let result = [];
   for (let i = 0; i < arr.length; i++) {
-    if (arr[i] > largest) {
-      largest = arr[i];
+    let largest = arr[i][0];
+
+    for (let k = 0; k < arr[i].length; k++) {
+      if (arr[i][k] > largest) {
+        largest = arr[i][k];
+      }
     }
+    result.push(largest);
   }
-  return largest;
+  return result;
 }
 
-console.log(largestOfAll([9, 21, 41, 6, 22]));
+console.log(
+  largestOfAll([
+    [9, 211, 41, 106, 22],
+    [99, 118, 64, 3, 7],
+    [67, 54, 41, 170, 12],
+    [98, 89, 4, 9, 99],
+  ])
+);
