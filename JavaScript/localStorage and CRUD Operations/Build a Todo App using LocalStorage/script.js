dfb1b7237fde25704d6327eaf3452a35b791aa7c
Moiz Siddiqui <moizsidd05@gmail.com>
2026-05-15 21:35:31 +0500

    coded a workshop in js section basic regex (spam filter)

--- JavaScript/localStorage and CRUD Operations/Build a Todo App using LocalStorage/script.js
+++ JavaScript/localStorage and CRUD Operations/Build a Todo App using LocalStorage/script.js
@@ -1,14 +1,14 @@
 const taskForm = document.getElementById("task-form");
-const confirmCloseDialog = document.getElementById("confirm-close-dialog");
+const cancelBtn = document.getElementById("cancel-btn");
+const dateInput = document.getElementById("date-input");
+const discardBtn = document.getElementById("discard-btn");
+const titleInput = document.getElementById("title-input");
+const tasksContainer = document.getElementById("tasks-container");
+const descriptionInput = document.getElementById("description-input");
 const openTaskFormBtn = document.getElementById("open-task-form-btn");
 const closeTaskFormBtn = document.getElementById("close-task-form-btn");
+const confirmCloseDialog = document.getElementById("confirm-close-dialog");
 const addOrUpdateTaskBtn = document.getElementById("add-or-update-task-btn");
-const cancelBtn = document.getElementById("cancel-btn");
-const discardBtn = document.getElementById("discard-btn");
-const tasksContainer = document.getElementById("tasks-container");
-const titleInput = document.getElementById("title-input");
-const dateInput = document.getElementById("date-input");
-const descriptionInput = document.getElementById("description-input");
 
 const taskData = JSON.parse(localStorage.getItem("data")) || [];
 let currentTask = {};
@@ -22,7 +22,9 @@ const addOrUpdateTask = () => {
     alert("Please provide a title");
     return;
   }
+
   const dataArrIndex = taskData.findIndex((item) => item.id === currentTask.id);
+
   const taskObj = {
     id: `${titleInput.value.toLowerCase().split(" ").join("-")}-${Date.now()}`,
     title: titleInput.value,
