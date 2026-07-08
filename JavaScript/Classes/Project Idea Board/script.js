5eefa4393296faf836a1d293096a503f2a0d9e0c
Moiz Siddiqui <moizsidd05@gmail.com>
2026-07-08 23:26:49 +0500

    coded a lab in js section classes, project idea board

---
+++ JavaScript/Classes/Project Idea Board/script.js
@@ -0,0 +1,58 @@
+const projectStatus = {
+  PENDING: {
+    description: "Pending Execution",
+  },
+  SUCCESS: {
+    description: "Executed Successfully",
+  },
+  FAILURE: {
+    description: "Execution Failed",
+  },
+};
+
+class ProjectIdea {
+  constructor(title, description) {
+    this.title = title;
+    this.description = description;
+    this.status = projectStatus.PENDING;
+  }
+
+  updateProjectStatus(newStatus) {
+    if (newStatus && typeof newStatus === "object") {
+      this.status = newStatus;
+    }
+  }
+}
+
+class ProjectIdeaBoard {
+  constructor(title) {
+    this.title = title;
+    this.ideas = [];
+  }
+
+  pin(projectIdea) {
+    if (projectIdea instanceof ProjectIdea) {
+      this.ideas.push(projectIdea);
+    }
+  }
+
+  unpin(projectIdea) {
+    this.ideas = this.ideas.filter(
+      (idea) => idea !== projectIdea && idea.title !== projectIdea.title,
+    );
+  }
+
+  count() {
+    return this.ideas.length;
+  }
+
+  formatToString() {
+    let result = `${this.title} has ${this.count()} idea(s)\n`;
+
+    this.ideas.forEach((idea) => {
+      result += `${idea.title} (${idea.status.description}) - ${idea.description}\n`;
+    });
+
+    return result;
+  }
+}
