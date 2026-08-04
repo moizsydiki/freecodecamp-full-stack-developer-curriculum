8ff3a1da1a778d110bbde205b063474176c38204
Moiz Siddiqui <moizsidd05@gmail.com>
2026-08-04 08:18:19 +0500

    coded a workshop in js section funcitonal programming, ingredient converter

--- JavaScript/Dynamic Programming/PrimeNumberSumCalculator.js
+++ JavaScript/Dynamic Programming/PrimeNumberSumCalculator.js
@@ -1,26 +1,26 @@
 const sumPrimes = (num) => {
-  if(num < 2) {
-    return 0
+  if (num < 2) {
+    return 0;
   }
 
   let sum = 0;
 
-  for(let i = 2; i <= num; i++) {
+  for (let i = 2; i <= num; i++) {
     let isPrime = true;
-    for(let j = 2; j <= Math.sqrt(i); j++) {
-      if(i % j === 0) {
-        isPrime = false;
-        break
-      }
 
+    for (let j = 2; j <= Math.sqrt(i); j++) {
+      if (i % j === 0) {
+        isPrime = false;
+        break;
+      }
     }
 
-    if(isPrime) {
+    if (isPrime) {
       sum += i;
     }
   }
 
   return sum;
-}
+};
 
-console.log(sumPrimes(7))
+console.log(sumPrimes(7));
